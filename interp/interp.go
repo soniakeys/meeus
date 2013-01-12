f306212363f5a815cd1eb5a06d1082f267676ae8
Sonia Keys <soniakeys@gmail.com>
2013-01-12 13:09:37 -0500

    complete interpolation chapter

--- interp/interp.go
+++ interp/interp.go
@@ -8,22 +8,37 @@
 // These functions interpolate from a table of equidistant x values
 // and corresponding y values.  Since the x values are equidistant,
 // only the first and last values are supplied as arguments.  The interior
-// values are implicit.
+// x values are implicit.
 //
 // All y values must be supplied however.  They are passed as a slice,
-// so the length of y is fixed.  For Len3 functions it must be 3 and for
+// and the length of y is fixed.  For Len3 functions it must be 3 and for
 // Len5 functions it must be 5.
+//
+// For these Len3 and Len5 functions, Meeus notes the importance of choosing
+// the 3 or 5 rows of a larger table that will minimize the interpolating
+// factor n.  He does not provide algorithms for doing this however, so none
+// are provided here.  For Interpolate, you must use your own algorithm to
+// select 3 or 5 rows that will have a middle row with x value closest to
+// the argument.  For Extremum and Zero, you must use your own algorithm to
+// select 3 or 5 rows that will have a middle row with x value closest to
+// the expected result.
 package interp
 
 import (
 	"errors"
 	"math"
+
+	"github.com/soniakeys/meeus"
 )
 
+// Error values returned by functions in this package.  Defined here to help
+// testing for specific errors.
 var (
 	ErrorNot3        = errors.New("Argument yTable must be length 3")
-	ErrorNoXRange    = errors.New("Argument x3 cannot equal x1")
-	ErrorXOutOfRange = errors.New("Argument x outside of range x1 to x3")
+	ErrorNot4        = errors.New("Argument yTable must be length 4")
+	ErrorNot5        = errors.New("Argument yTable must be length 5")
+	ErrorNoXRange    = errors.New("Argument x3 (or x5) cannot equal x1")
+	ErrorXOutOfRange = errors.New("Argument x outside of range x1 to x3 (or x5)")
 	ErrorNoExtremum  = errors.New("No extremum in table")
 	ErrorZeroOutside = errors.New("Zero falls outside of table")
 	ErrorNoConverge  = errors.New("Failure to converge")
@@ -33,7 +48,7 @@ var (
 // by taking second differences.
 //
 // Function returns interpolated y value for argument x, as long as x is
-// within the table.  X3 must be > x1,
+// within the table.
 func Len3Interpolate(x, x1, x3 float64, yTable []float64, allowExtrapolate bool) (y float64, err error) {
 	// AA p. 24.
 	if len(yTable) != 3 {
@@ -111,24 +126,257 @@ func Len3Zero(x1, x3 float64, yTable []float64, strong bool) (x float64, err err
 	a := yTable[1] - yTable[0]
 	b := yTable[2] - yTable[1]
 	c := b - a
-	var n0, n1 float64
-	for limit := 0; limit < 50; limit++ {
-		if strong {
-			// AA p. 27.
-			n1 = n0 - (2*yTable[1]+n0*(a+b+c*n0))/(a+b+2*c*n0)
-		} else {
-			n1 = -2 * yTable[1] / (a + b + c*n0)
+	var f func(n0 float64) (n1 float64)
+	if strong {
+		f = func(n0 float64) float64 {
+			return n0 - (2*yTable[1]+n0*(a+b+c*n0))/(a+b+2*c*n0)
 		}
+	} else {
+		f = func(n0 float64) float64 {
+			return -2 * yTable[1] / (a + b + c*n0)
+		}
+	}
+	n0, ok := iterate(0, f)
+	if !ok {
+		return 0, ErrorNoConverge
+	}
+	if n0 > 1 || n0 < -1 {
+		return 0, ErrorZeroOutside
+	}
+	return .5 * ((x3 + x1) + (x3-x1)*n0), nil // success
+}
+
+func iterate(n0 float64, f func(n0 float64) (n1 float64)) (n1 float64, ok bool) {
+	for limit := 0; limit < 50; limit++ {
+		n1 = f(n0)
 		if math.IsInf(n1, 0) || math.IsNaN(n1) {
 			break // failure to converge
 		}
 		if math.Abs((n1-n0)/n0) < 1e-15 {
-			if n1 > 1 || n1 < -1 {
-				return 0, ErrorZeroOutside
-			}
-			return .5 * ((x3 + x1) + (x3-x1)*n1), nil // success
+			return n1, true // success
 		}
 		n0 = n1
 	}
-	return 0, ErrorNoConverge
+	return 0, false // failure to converge
+}
+
+// Len4Half interpolates a center value from a table of four rows.
+func Len4Half(yTable []float64) (float64, error) {
+	if len(yTable) != 4 {
+		return 0, ErrorNot4
+	}
+	return (9*(yTable[1]+yTable[2]) - yTable[0] - yTable[3]) / 16, nil
+}
+
+// Len5Interpolate interpolates from a table of five rows
+// by taking fouth differences.
+//
+// Function returns interpolated y value for argument x, as long as x is
+// within the table.
+func Len5Interpolate(x, x1, x5 float64, yTable []float64, allowExtrapolate bool) (y float64, err error) {
+	// AA p. 28.
+	if len(yTable) != 5 {
+		return 0, ErrorNot5
+	}
+	if x5 == x1 {
+		return 0, ErrorNoXRange
+	}
+	if !allowExtrapolate {
+		if x5 > x1 {
+			// increasing x
+			if x < x1 || x > x5 {
+				return 0, ErrorXOutOfRange
+			}
+		} else {
+			// decreasing x
+			if x > x1 || x < x5 {
+				return 0, ErrorXOutOfRange
+			}
+		}
+	}
+	l5 := newLen5diffs(yTable)
+	n := (4*x - 2*(x1+x5)) / (x5 - x1)
+	return l5.eval(n), nil
+}
+
+// Len5Extremum finds the extremum in a table of 5 rows, interpolating
+// by second differences.
+//
+// It returns the x and y values at the extremum.
+func Len5Extremum(x1, x5 float64, yTable []float64) (x, y float64, err error) {
+	// AA p. 29.
+	if len(yTable) != 5 {
+		return 0, 0, ErrorNot5
+	}
+	if x5 == x1 {
+		return 0, 0, ErrorNoXRange
+	}
+	l5 := newLen5diffs(yTable)
+	nCoeff := []float64{
+		6*(l5.B+l5.C) - l5.H - l5.J,
+		0,
+		3 * (l5.H + l5.K),
+		2 * l5.K,
+	}
+	den := l5.K - 12*l5.F
+	n0, ok := iterate(0, func(n0 float64) float64 {
+		return meeus.Horner(n0, nCoeff) / den
+	})
+	if !ok {
+		return 0, 0, ErrorNoConverge
+	}
+	x = .5*(x5+x1) + .25*(x5-x1)*n0
+	return x, l5.eval(n0), nil
+}
+
+type len5diffs struct {
+	y3, A, B, C, D, E, F, G, H, J, K float64
+}
+
+func newLen5diffs(y []float64) *len5diffs {
+	l5 := new(len5diffs)
+	l5.y3 = y[2]
+
+	l5.A = y[1] - y[0]
+	l5.B = y[2] - y[1]
+	l5.C = y[3] - y[2]
+	l5.D = y[4] - y[3]
+
+	l5.E = l5.B - l5.A
+	l5.F = l5.C - l5.B
+	l5.G = l5.D - l5.C
+
+	l5.H = l5.F - l5.E
+	l5.J = l5.G - l5.F
+
+	l5.K = l5.J - l5.H
+	return l5
+}
+
+func (l5 *len5diffs) eval(n float64) float64 {
+	return meeus.Horner(n, []float64{
+		l5.y3,
+		(l5.B+l5.C)/2 - (l5.H+l5.J)/12,
+		l5.F/2 - l5.K/24,
+		(l5.H + l5.J) / 12,
+		l5.K / 24,
+	})
+}
+
+// Len5Zero finds the first zero in a table of five rows, interpolating
+// by fourth differences.
+//
+// It returns the x value that yields y=0.
+//
+// Argument strong switches between two strategies for the estimation step.
+// when iterating to converge on the zero.
+// Strong=false specifies a quick and dirty estimate that works well
+// for gentle curves, but can work poorly or fail on more dramatic curves.
+//
+// Strong=true specifies a more sophisticated and thus somewhat more
+// expensive estimate.  However, if the curve has quick changes, This estimate
+// will converge more reliably and in fewer steps, making it a better choice.
+func Len5Zero(x1, x5 float64, yTable []float64, strong bool) (x float64, err error) {
+	// AA p. 29.
+	if len(yTable) != 5 {
+		return 0, ErrorNot5
+	}
+	if x5 == x1 {
+		return 0, ErrorNoXRange
+	}
+	l5 := newLen5diffs(yTable)
+	var f func(n0 float64) (n1 float64)
+	if strong {
+		M := l5.K / 24
+		N := (l5.H + l5.J) / 12
+		P := l5.F/2 - M
+		Q := (l5.B+l5.C)/2 - N
+		numCoeff := []float64{l5.y3, Q, P, N, M}
+		denCoeff := []float64{Q, 2 * P, 3 * N, 4 * M}
+		f = func(n0 float64) float64 {
+			return n0 - meeus.Horner(n0, numCoeff)/meeus.Horner(n0, denCoeff)
+		}
+	} else {
+		numCoeff := []float64{
+			-24 * l5.y3,
+			0,
+			l5.K - 12*l5.F,
+			-2 * (l5.H + l5.J),
+			-l5.K,
+		}
+		den := 12*(l5.B+l5.C) - 2*(l5.H+l5.J)
+		f = func(n0 float64) float64 {
+			return meeus.Horner(n0, numCoeff) / den
+		}
+	}
+	n0, ok := iterate(0, f)
+	if !ok {
+		return 0, ErrorNoConverge
+	}
+	if n0 > 2 || n0 < -2 {
+		return 0, ErrorZeroOutside
+	}
+	x = .5*(x5+x1) + .25*(x5-x1)*n0
+	return x, nil
+}
+
+// Lagrange performs interpolation with unequally-spaced abscissae.
+//
+// Given a table of X and Y values, interpolate a new y value for argument x.
+//
+// X values in the table do not have to be equally spaced; they do not even
+// have to be in order.  They must however, be distinct.
+func Lagrange(x float64, table []struct{ X, Y float64 }) (y float64) {
+	// method of BASIC program, p. 33.
+	sum := 0.
+	for i := range table {
+		xi := table[i].X
+		prod := 1.
+		for j := range table {
+			if i != j {
+				xj := table[j].X
+				prod *= (x - xj) / (xi - xj)
+			}
+		}
+		sum += table[i].Y * prod
+	}
+	return sum
+}
+
+// LagrangePoly uses the formula of Lagrange to produce an interpolating
+// polynomial.
+//
+// X values in the table do not have to be equally spaced; they do not even
+// have to be in order.  They must however, be distinct.
+//
+// The returned polynomial will be of degree n-1 where n is the number of rows
+// in the table.  It can be evaluated for x using meeus.Horner.
+func LagrangePoly(table []struct{ X, Y float64 }) []float64 {
+	// Method not fully described by Meeus, but needed for numerical solution
+	// to Example 3.g.
+	sum := make([]float64, len(table))
+	prod := make([]float64, len(table))
+	last := len(table) - 1
+	for i := range table {
+		xi := table[i].X
+		yi := table[i].Y
+		prod[last] = 1
+		den := 1.
+		n := last
+		for j := range table {
+			if i != j {
+				xj := table[j].X
+				prod[n-1] = prod[n] * -xj
+				for k := n; k < last; k++ {
+					prod[k] -= prod[k+1] * xj
+				}
+				n--
+				den *= (xi - xj)
+			}
+		}
+		for j, pj := range prod {
+			sum[j] += yi * pj / den
+		}
+	}
+	return sum
 }
--- interp/interp_test.go
+++ interp/interp_test.go
@@ -1,6 +1,7 @@
 package interp_test
 
 import (
+	"fmt"
 	"math"
 	"testing"
 
@@ -8,8 +9,8 @@ import (
 	"github.com/soniakeys/meeus/interp"
 )
 
-// Example 3.a, p. 25.
-func TestLen3Interpolate(t *testing.T) {
+func ExampleLen3Interpolate() {
+	// Example 3.a, p. 25.
 	x1 := 7.
 	x3 := 9.
 	yTable := []float64{
@@ -17,18 +18,19 @@ func TestLen3Interpolate(t *testing.T) {
 		.877366,
 		.870531,
 	}
-	x := 8 + (4+21/60.)/24.
+	x := 8 + (4+21/60.)/24 // quick computation of the 8th day at 4:21
 	y, err := interp.Len3Interpolate(x, x1, x3, yTable, false)
 	if err != nil {
-		t.Fatal(err)
+		fmt.Println(err)
+		return
 	}
-	if math.Abs(y-.876125) > 1e-6 {
-		t.Fatal("y:", y)
-	}
+	fmt.Printf("%.6f\n", y)
+	// Output:
+	// 0.876125
 }
 
-// Example 3.b p. 26.
-func TestLen3Extremum(t *testing.T) {
+func ExampleLen3Extremum() {
+	// Example 3.b, p. 26.
 	x1 := 12.
 	x3 := 20.
 	yTable := []float64{
@@ -38,21 +40,24 @@ func TestLen3Extremum(t *testing.T) {
 	}
 	x, y, err := interp.Len3Extremum(x1, x3, yTable)
 	if err != nil {
-		t.Fatal(err)
+		fmt.Println(err)
+		return
 	}
-	// (Meeus looses a decimal place here by rounding nm before multiplying
-	// by dx.)
-	if math.Abs(x-17.5864) > 1e-4 {
-		t.Fatal("wrong x:", x)
-	}
-	if math.Abs(y-1.3812030) > 1e-7 {
-		t.Fatal("wrong y", y)
-	}
+	fmt.Printf("distance:  %.7f\n", y)
+	fmt.Printf("date:     %.4f\n", x)
+	// Output:
+	// distance:  1.3812030
+	// date:     17.5864
 }
 
-func TestLen3Zero(t *testing.T) {
+// (Note on 17.5864, just above:  Meeus looses a decimal place by rounding nm
+// before multiplying by dx.)
+
+func ExampleLen3Zero() {
+	// Example 3.c, p. 26.
 	x1 := 26.
 	x3 := 28.
+	// work in degrees
 	yTable := []float64{
 		meeus.DMSToDeg(true, 0, 28, 13.4) * 180 / math.Pi,
 		meeus.DMSToDeg(false, 0, 6, 46.3) * 180 / math.Pi,
@@ -60,22 +65,145 @@ func TestLen3Zero(t *testing.T) {
 	}
 	x, err := interp.Len3Zero(x1, x3, yTable, false)
 	if err != nil {
-		t.Fatal(err)
+		fmt.Println(err)
+		return
 	}
-	if math.Abs(x-26.79873) > 1e-5 {
-		t.Fatal("x:", x)
-	}
+	fmt.Printf("%.5f\n", x)
+	// Output:
+	// 26.79873
 }
 
-func TestLen3ZeroStrong(t *testing.T) {
+func ExampleLen3Zero_strong() {
+	// Example 3.d, p. 27.
 	x1 := -1.
 	x3 := 1.
 	yTable := []float64{-2, 3, 2}
 	x, err := interp.Len3Zero(x1, x3, yTable, true)
 	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("%.12f\n", x)
+	// Output:
+	// -0.720759220056
+}
+
+func ExampleLen5Interpolate() {
+	// Example 3.e, p. 28.
+	x1 := 27.
+	x5 := 29.
+	// work in radians to get answer in radians
+	var pTable [5]meeus.Angle
+	pTable[0].SetDMS(false, 0, 54, 36.125)
+	pTable[1].SetDMS(false, 0, 54, 24.606)
+	pTable[2].SetDMS(false, 0, 54, 15.486)
+	pTable[3].SetDMS(false, 0, 54, 08.694)
+	pTable[4].SetDMS(false, 0, 54, 04.133)
+	yTable := []float64{
+		pTable[0].Rad,
+		pTable[1].Rad,
+		pTable[2].Rad,
+		pTable[3].Rad,
+		pTable[4].Rad,
+	}
+	x := 28 + (3+20./60)/24
+	y, err := interp.Len5Interpolate(x, x1, x5, yTable, false)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// radians easy to format
+	fmt.Printf("%.3d", &meeus.Angle{Rad: y})
+	// Output:
+	// 54′13″.369
+}
+
+// Exercise, p. 30.
+func TestLen5Zero(t *testing.T) {
+	x1 := 25.
+	x5 := 29.
+	yTable := []float64{
+		meeus.DMSToDeg(true, 1, 11, 21.23),
+		meeus.DMSToDeg(true, 0, 28, 12.31),
+		meeus.DMSToDeg(false, 0, 16, 07.02),
+		meeus.DMSToDeg(false, 1, 01, 00.13),
+		meeus.DMSToDeg(false, 1, 45, 46.33),
+	}
+	z, err := interp.Len5Zero(x1, x5, yTable, false)
+	if err != nil {
 		t.Fatal(err)
 	}
-	if math.Abs(x-(-.720759220056)) > 1e-12 {
-		t.Fatal("x:", x)
+	if math.Abs(z-26.638587) > 1e-6 {
+		t.Fatal(z)
 	}
+	// using three central values
+	z, err = interp.Len3Zero(26, 28, yTable[1:4], false)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if math.Abs(z-(26.638587-.000753)) > 1e-6 {
+		t.Fatal(z)
+	}
+}
+
+func ExampleLen4Half() {
+	// Example 3.f, p. 32.
+	var hTable [4]meeus.RA
+	hTable[0].SetHMS(10, 18, 48.732)
+	hTable[1].SetHMS(10, 23, 22.835)
+	hTable[2].SetHMS(10, 27, 57.247)
+	hTable[3].SetHMS(10, 32, 31.983)
+	half, err := interp.Len4Half([]float64{
+		hTable[0].Rad,
+		hTable[1].Rad,
+		hTable[2].Rad,
+		hTable[3].Rad,
+	})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("%.3d", &meeus.RA{Rad: half})
+	// Output:
+	// 10ʰ25ᵐ40ˢ.001
+}
+
+// exercise, p. 34.
+func TestLagrange(t *testing.T) {
+	table := []struct{ X, Y float64 }{
+		{29.43, .4913598528},
+		{30.97, .5145891926},
+		{27.69, .4646875083},
+		{28.11, .4711658342},
+		{31.58, .5236885653},
+		{33.05, .5453707057},
+	}
+	if math.Abs(interp.Lagrange(30, table)-.5) > 1e-5 {
+		t.Fatal(30)
+	}
+	if math.Abs(interp.Lagrange(0, table)) > 1e-4 {
+		t.Fatal(0)
+	}
+	if math.Abs(interp.Lagrange(90, table)-1) > 1e-4 {
+		t.Fatal(90)
+	}
+}
+
+func ExampleLagrangePoly() {
+	// Example 3.g, p, 34.
+	table := []struct{ X, Y float64 }{
+		{1, -6},
+		{3, 6},
+		{4, 9},
+		{6, 15},
+	}
+	p := interp.LagrangePoly(table)
+	for _, c := range p {
+		fmt.Printf("%.0f\n", c*5)
+	}
+	// Output:
+	// -87
+	// 69
+	// -13
+	// 1
 }
