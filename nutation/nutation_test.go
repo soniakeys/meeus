b05267b72cf6f8b294f87b0e7fa068b0859d3905
Sonia Keys <soniakeys@gmail.com>
2013-01-16 20:03:29 -0500

    add nutation

---
+++ nutation/nutation_test.go
@@ -0,0 +1,58 @@
+package nutation_test
+
+import (
+	"fmt"
+	"math"
+	"testing"
+
+	"github.com/soniakeys/meeus"
+	"github.com/soniakeys/meeus/julian"
+	"github.com/soniakeys/meeus/nutation"
+)
+
+func ExampleNutation() {
+	// Example 22.a, p. 148.
+	jd := julian.CalendarGregorianToJD(1987, 4, 10)
+	Δψ, Δε := nutation.Nutation(jd)
+	ε0 := nutation.MeanObliquityIAU1980(jd)
+	ε := ε0 + Δε
+	fmt.Printf("%+.3d\n", meeus.NewFmtAngle(Δψ))
+	fmt.Printf("%+.3d\n", meeus.NewFmtAngle(Δε))
+	fmt.Printf("%.3d\n", meeus.NewFmtAngle(ε0))
+	fmt.Printf("%.3d\n", meeus.NewFmtAngle(ε))
+	// Output:
+	// -3″.788
+	// +9″.443
+	// 23°26′27″.407
+	// 23°26′36″.850
+}
+
+func TestApproxNutation(t *testing.T) {
+	jd := julian.CalendarGregorianToJD(1987, 4, 10)
+	Δψ, Δε := nutation.ApproxNutation(jd)
+	if math.Abs(Δψ*(180/math.Pi)*3600+3.788) > .5 {
+		t.Fatal(Δψ * (180 / math.Pi) * 3600)
+	}
+	if math.Abs(Δε*(180/math.Pi)*3600-9.443) > .1 {
+		t.Fatal(Δε * (180 / math.Pi) * 3600)
+	}
+}
+
+func TestIAUvsLaskar(t *testing.T) {
+	for _, y := range []int{1000, 2000, 3000} {
+		jd := julian.CalendarGregorianToJD(y, 0, 0)
+		i := nutation.MeanObliquityIAU1980(jd)
+		l := nutation.MeanObliquityLaskar(jd)
+		if math.Abs(i-l)*(180/math.Pi)*3600 > 1 {
+			t.Fatal(y)
+		}
+	}
+	for _, y := range []int{0, 4000} {
+		jd := julian.CalendarGregorianToJD(y, 0, 0)
+		i := nutation.MeanObliquityIAU1980(jd)
+		l := nutation.MeanObliquityLaskar(jd)
+		if math.Abs(i-l)*(180/math.Pi)*3600 > 10 {
+			t.Fatal(y)
+		}
+	}
+}
