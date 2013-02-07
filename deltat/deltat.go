fa368591554a70714c8b439d656158493b9565bf
Sonia Keys <soniakeys@gmail.com>
2013-02-07 12:58:20 -0500

    Add Table10A, rename other functions to remove stutter.

--- deltat/deltat.go
+++ deltat/deltat.go
@@ -8,16 +8,16 @@ package deltat
 import (
 	"github.com/soniakeys/meeus/common"
 	"github.com/soniakeys/meeus/hints"
+	"github.com/soniakeys/meeus/interp"
+	"github.com/soniakeys/meeus/julian"
 )
 
-// Table10A encodes ΔT = TD - UT for the range of years TableYear1 to
-// TableYearN.
-//
-// See example at DeltaT1900to1997.
+// table10A encodes ΔT = TD - UT for the range of years tableYear1 to
+// tableYearN.
 var (
-	TableYear1 = 1620.
-	TableYearN = 2010.
-	Table10A   = []float64{
+	tableYear1 = 1620.
+	tableYearN = 2010.
+	table10A   = []float64{
 		121.0, 112.0, 103.0, 95.0, 88.0, 82.0, 77.0, 72.0, 68.0, 63.0,
 		60.0, 56.0, 53.0, 51.0, 48.0, 46.0, 44.0, 42.0, 40.0, 38.0,
 		35.0, 33.0, 31.0, 29.0, 26.0, 24.0, 22.0, 20.0, 18.0, 16.0,
@@ -44,6 +44,23 @@ var (
 		63.8, 64.3, 64.6, 64.8, 65.5, 66.1}
 )
 
+func Interp10A(jde float64) float64 {
+	// kind of crazy, working in calendar years.
+	y, m, d := julian.JDToCalendar(jde)
+	l := julian.LeapYearGregorian(y)
+	yl := 365.
+	if l {
+		yl++
+	}
+	yf := float64(y) + float64(julian.DayOfYear(y, m, int(d+.5), l))/yl
+	x1, x3, yTable := interp.Slice(yf, tableYear1, tableYearN, table10A, 3)
+	dt, err := interp.Len3Interpolate(yf, x1, x3, yTable, false)
+	if err != nil {
+		panic(err)
+	}
+	return dt
+}
+
 // c2000 returns centuries from calendar year 2000.0.
 //
 // Arg should be a calendar year.
@@ -51,22 +68,22 @@ func c2000(y float64) float64 {
 	return (y - 2000) * .01
 }
 
-// DeltaTBefore948 returns a polynomial approximation valid for calendar
+// PolyBefore948 returns a polynomial approximation valid for calendar
 // years before 948.
-func DeltaTBefore948(year float64) float64 {
+func PolyBefore948(year float64) float64 {
 	return hints.Horner(c2000(year), []float64{2177, 497, 44.1})
 }
 
-// DeltaT948to1600 returns a polynomial approximation valid for calendar
+// Poly948to1600 returns a polynomial approximation valid for calendar
 // years 948 to 1600.
-func DeltaT948to1600(year float64) float64 {
+func Poly948to1600(year float64) float64 {
 	return hints.Horner(c2000(year), []float64{102, 102, 25.3})
 }
 
-// DeltaTAfter2000 returns a polynomial approximation valid for calendar
+// PolyAfter2000 returns a polynomial approximation valid for calendar
 // years after 2000.
-func DeltaTAfter2000(year float64) float64 {
-	return DeltaT948to1600(year) + .37*(year-2100)
+func PolyAfter2000(year float64) float64 {
+	return Poly948to1600(year) + .37*(year-2100)
 }
 
 // jc1900 returns julian centuries from the epoch J1900.0
@@ -76,33 +93,33 @@ func jc1900(jde float64) float64 {
 	return (jde - common.J1900) / common.JulianCentury
 }
 
-// DeltaT1800to1997 returns a polynomial approximation valid for years
+// Poly1800to1997 returns a polynomial approximation valid for years
 // 1800 to 1997.
 //
 // The accuracy is within 2.3 seconds.
-func DeltaT1800to1997(jde float64) float64 {
+func Poly1800to1997(jde float64) float64 {
 	return hints.Horner(jc1900(jde), []float64{
 		-1.02, 91.02, 265.90, -839.16, -1545.20,
 		3603.62, 4385.98, -6993.23, -6090.04,
 		6298.12, 4102.86, -2137.64, -1081.51})
 }
 
-// DeltaT1800to1899 returns a polynomial approximation valid for years
+// Poly1800to1899 returns a polynomial approximation valid for years
 // 1800 to 1899.
 //
 // The accuracy is within 0.9 seconds.
-func DeltaT1800to1899(jde float64) float64 {
+func Poly1800to1899(jde float64) float64 {
 	return hints.Horner(jc1900(jde), []float64{
 		-2.50, 228.95, 5218.61, 56282.84, 324011.78,
 		1061660.75, 2087298.89, 2513807.78,
 		1818961.41, 727058.63, 123563.95})
 }
 
-// DeltaT1900to1997 returns a polynomial approximation valid for years
+// Poly1900to1997 returns a polynomial approximation valid for years
 // 1900 to 1997.
 //
 // The accuracy is within 0.9 seconds.
-func DeltaT1900to1997(jde float64) float64 {
+func Poly1900to1997(jde float64) float64 {
 	return hints.Horner(jc1900(jde), []float64{
 		-2.44, 87.24, 815.20, -2637.80, -18756.33,
 		124906.15, -303191.19, 372919.88,
--- deltat/deltat_test.go
+++ deltat/deltat_test.go
@@ -8,47 +8,37 @@ import (
 
 	"github.com/soniakeys/meeus/common"
 	"github.com/soniakeys/meeus/deltat"
-	"github.com/soniakeys/meeus/interp"
 	"github.com/soniakeys/meeus/julian"
 )
 
-func ExampleDeltaT1900to1997_table() {
+func ExampleInterp10A() {
 	// Example 10.a, p. 78.
-	calYear := 1977 + float64(julian.DayOfYear(1977, 2, 18, false))/365
-	fmt.Printf("calendar year %.1f\n", calYear)
-	x1, x3, yTable := interp.Slice(calYear,
-		deltat.TableYear1, deltat.TableYearN, deltat.Table10A, 3)
-	dt, err := interp.Len3Interpolate(calYear, x1, x3, yTable, false)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	dt := deltat.Interp10A(julian.CalendarGregorianToJD(1977, 2, 18))
 	fmt.Printf("%+.1f seconds\n", dt)
 	// Output:
-	// calendar year 1977.1
 	// +46.8 seconds
 }
 
-func ExampleDeltaT1900to1997_polynomial() {
+func ExamplePoly1900to1997() {
 	// Example 10.a, p. 78.
 	jd := julian.TimeToJD(time.Date(1977, 2, 18, 3, 37, 40, 0, time.UTC))
 	year := common.JDEToJulianYear(jd)
 	fmt.Printf("julian year %.1f\n", year)
-	fmt.Printf("%+.1f seconds\n", deltat.DeltaT1900to1997(jd))
+	fmt.Printf("%+.1f seconds\n", deltat.Poly1900to1997(jd))
 	// Output:
 	// julian year 1977.1
 	// +47.1 seconds
 }
 
-func ExampleDeltaTBefore948() {
+func ExamplePolyBefore948() {
 	// Example 10.b, p. 80.
-	fmt.Printf("%+.0f seconds\n", deltat.DeltaTBefore948(333.1))
+	fmt.Printf("%+.0f seconds\n", deltat.PolyBefore948(333.1))
 	// Output:
 	// +6146 seconds
 }
 
 // Table 10.A p. 79 provides a way to test these polynomials
-func TestDeltaT1800to1997(t *testing.T) {
+func TestPoly1800to1997(t *testing.T) {
 	for _, tp := range []struct {
 		year int
 		ΔT   float64
@@ -58,14 +48,14 @@ func TestDeltaT1800to1997(t *testing.T) {
 		{1996, 61.6},
 	} {
 		jd := julian.CalendarGregorianToJD(tp.year, 0, 0)
-		ΔT := deltat.DeltaT1800to1997(jd)
+		ΔT := deltat.Poly1800to1997(jd)
 		if math.Abs(ΔT-tp.ΔT) > 2.3 {
 			t.Fatalf("%#v, got %.1f", tp, ΔT)
 		}
 	}
 }
 
-func TestDeltaT1800to1899(t *testing.T) {
+func TestPoly1800to1899(t *testing.T) {
 	for _, tp := range []struct {
 		year int
 		ΔT   float64
@@ -75,16 +65,16 @@ func TestDeltaT1800to1899(t *testing.T) {
 		{1898, -4.7},
 	} {
 		jd := julian.CalendarGregorianToJD(tp.year, 0, 0)
-		if math.Abs(deltat.DeltaT1800to1899(jd)-tp.ΔT) > 1 {
+		if math.Abs(deltat.Poly1800to1899(jd)-tp.ΔT) > 1 {
 			t.Fatalf("%#v", tp)
 		}
 	}
 }
 
-func TestDeltaT1900to1997(t *testing.T) {
+func TestPoly1900to1997(t *testing.T) {
 	for y := 1900; y < 1998; y += 2 {
 		jd := julian.CalendarGregorianToJD(y, 0, 0)
-		t.Logf("%d %.2f  %.1f", y, jd, deltat.DeltaT1900to1997(jd))
+		t.Logf("%d %.2f  %.1f", y, jd, deltat.Poly1900to1997(jd))
 	}
 	for _, tp := range []struct {
 		year int
@@ -95,7 +85,7 @@ func TestDeltaT1900to1997(t *testing.T) {
 		{1996, 61.6},
 	} {
 		jd := julian.CalendarGregorianToJD(tp.year, 0, 0)
-		ΔT := deltat.DeltaT1900to1997(jd)
+		ΔT := deltat.Poly1900to1997(jd)
 		if math.Abs(ΔT-tp.ΔT) > 1 {
 			t.Fatalf("%#v, got %.1f", tp, ΔT)
 		}
