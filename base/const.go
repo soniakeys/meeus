76471f12cb60b6e39db90bf2894482fe4f1cbfeb
Sonia Keys <soniakeys@gmail.com>
2013-07-26 16:14:20 -0400

    add obliquity constants

--- base/const.go
+++ base/const.go
@@ -4,4 +4,12 @@
 package base
 
 // K is the Gaussian gravitational constant.
+//
+// From ch 33, p. 228, for example
 const K = .01720209895
+
+// SOblJ2000, COblJ2000 are sine and cosine of obliquity at J2000.
+//
+// From ch 33, p. 228, for example
+const SOblJ2000 = .397777156
+const COblJ2000 = .917482062
