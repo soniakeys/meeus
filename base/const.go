c1a5c77e0a5cba8b7a41b53fc879eee1b4718810
Sonia Keys <soniakeys@gmail.com>
2013-08-16 14:02:28 -0400

    group constants for sensible documentation

--- base/const.go
+++ base/const.go
@@ -11,8 +11,10 @@ const K = .01720209895
 // SOblJ2000, COblJ2000 are sine and cosine of obliquity at J2000.
 //
 // From ch 33, p. 228, for example
-const SOblJ2000 = .397777156
-const COblJ2000 = .917482062
+const (
+	SOblJ2000 = .397777156
+	COblJ2000 = .917482062
+)
 
 // LightTime returns time for light to travel a given distance.
 //
